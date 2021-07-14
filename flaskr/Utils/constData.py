cc7cccbad3fe7e9ebcc535dd8e19ba4f1b35b5b5
DanhLe1412 <Dragonfly0165@gmail.com>
2021-07-14 21:41:01 +0700

    add requirement.txt

---
+++ flaskr/Utils/constData.py
@@ -0,0 +1,8 @@
+URL = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart"
+
+headers = {
+        'x-rapidapi-key': "f4db027b4amshc079fa1945a1f64p1d6b71jsn2ae98bd36409",
+        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
+        }
+
+
